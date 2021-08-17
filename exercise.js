cba67d4440af00c024455045f71bf71e9f94dd52
JC Ketterer <jcketterer18@gmail.com>
2021-08-16 22:20:07 -0700

    first commit

---
+++ exercise.js
@@ -0,0 +1,27 @@
+//ES5 Map Callback 
+
+function double(arr) {
+  return arr.map(function (val) {
+    return val * 2;
+  });
+}
+
+//ES2015 Arrow function callback
+
+const arr = [1, 2, 3, 4, 5]
+
+const doubleArr = arr.map((val) => val * 2);
+
+//replace all functions with arrow function
+function squareAndFindEvens(numbers) {
+  var squares = numbers.map(function (num) {
+    return num ** 2;
+  });
+  var evens = squares.filter(function (square) {
+    return square % 2 === 0;
+  });
+  return evens;
+}
+
+//Refactored
+squareAndFindEvens = numbers => numbers.map(num => num ** 2).filter(square => square % 2 === 0);
